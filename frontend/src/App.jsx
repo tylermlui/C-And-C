f790ef0a7f6b0504148ade3a02ace17539504a6e
tylerlui@gmail.com <114965892+tylermlui@users.noreply.github.com>
2024-08-28 02:06:26 -0700

    add react routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import './index.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LivePage from './pages/LivePage'; 
+import CartPage from './pages/CartPage';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/live" element={<LivePage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </Router>
+
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/CartPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return <div className="flex justify-center items-center bg-white h-screen">Cart Page</div>;
+};
+
+export default AboutPage;
